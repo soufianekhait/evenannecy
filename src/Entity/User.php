ee6f25ca92a3c2e0e7bfc46b5113f51da46257f2
Francisco Lopes <Francisco@Lopes.fr>
2019-03-15 14:30:02 +0000

    Add entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,6 +40,36 @@ class User implements UserInterface
      */
     private $subscription;
 
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $zip_code;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $country;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -126,6 +156,78 @@ class User implements UserInterface
     public function setSubscription(?Subscription $subscription): self
     {
         $this->subscription = $subscription;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $Street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(int $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $City): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
 
         return $this;
     }
