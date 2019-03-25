db12d7e91754da5c604dcfb64c237c3b545fea75
Soufiane KHAIT <45060816+soufianekhait@users.noreply.github.com>
2019-03-25 08:30:54 +0100

    Update scripts

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,31 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     let buttonBegin = document.getElementById("button-home");
+    var header = document.getElementById("navbar-wrapper");
+    var navbarUl = document.getElementById("navbar");
 
-    buttonBegin.addEventListener("mouseout", function(){
-        this.style.transition = "all 1s ease";
-    });
+    if(window.location.pathname == "/"){
+        buttonBegin.addEventListener("mouseout", function(){
+            this.style.transition = "all 1s ease";
+        });
 
-    window.addEventListener("scroll",function() {
-        var header = document.getElementById("navbar-wrapper");
-        var navbarUl = document.getElementById("navbar");
+        window.addEventListener("scroll",function(){
+            if(document.getElementById("title-home").getBoundingClientRect().top <= 100){
+                header.style.backgroundColor = "#222";
+                header.style.transitionDuration = "0.2s";
+                navbarUl.style.transform = "translateY(-10px)";
+                navbarUl.style.transitionDuration = "0.3s";
+            } else{
+                header.style.backgroundColor = "transparent";
+                header.style.transitionDuration = "0.2s";
+                navbarUl.style.transform = "translateY(0px)";
+                navbarUl.style.transitionDuration = "0.3s";
+            }
+        });
 
-        if(document.getElementById("title-home").getBoundingClientRect().top <= 100){
-            header.style.backgroundColor = "#222";
-            header.style.transitionDuration = "0.2s";
-            navbarUl.style.transform = "translateY(-10px)";
-            navbarUl.style.transitionDuration = "0.3s";
-        } else{
-            header.style.backgroundColor = "transparent";
-            header.style.transitionDuration = "0.2s";
-            navbarUl.style.transform = "translateY(0px)";
-            navbarUl.style.transitionDuration = "0.3s";
-        }
-    });
+        let homeBtn = document.getElementById("button-home");
 
-    let homeBtn = document.getElementById("button-home");
-
-    homeBtn.addEventListener("click", function(){
-        window.scrollTo(0,document.getElementById("subscription").offsetHeight);
-    })
+        homeBtn.addEventListener("click", function(){
+            window.scrollTo(0,document.getElementById("subscription").offsetHeight);
+        });
+    } else{
+        header.style.backgroundColor = "#222";
+        header.style.transitionDuration = "0.2s";
+        navbarUl.style.transform = "translateY(-10px)";
+        navbarUl.style.transitionDuration = "0.3s";
+    }
 });
